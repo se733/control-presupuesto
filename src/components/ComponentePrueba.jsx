46292fbd3b71cb8e66dcd794e4217a8098afcfa3
se733 <chavezsebas737@gmail.com>
2023-01-05 21:09:21 -0300

    lorem prueba

--- src/components/ComponentePrueba.jsx
+++ src/components/ComponentePrueba.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export const ComponentePrueba = () => {
   return (
-    <div>ComponentePrueba</div>
-  )
-}
+    <>
+      <div>ComponentePrueba</div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ex quod
+        eius iste ad recusandae nemo nisi totam. Autem ipsa dignissimos
+        repellendus ex omnis repellat eos fugiat fuga esse sequi?
+      </h1>
+    </>
+  );
+};
